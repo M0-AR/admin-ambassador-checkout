941fedb8b31a02ae019df9e58699f4accebea91b
m0-ar <s176492@student.dtu.dk>
2022-04-23 13:09:25 +0200

    Move SetPassword and ComparePassword methods to inside users model

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -5,7 +5,6 @@ import (
 	"admin-ambassador-checkout/src/models"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
-	"golang.org/x/crypto/bcrypt"
 	"strconv"
 	"time"
 )
@@ -24,15 +23,13 @@ func Register(c *fiber.Ctx) error {
 		})
 	}
 
-	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 12)
-
 	user := models.User{
 		FirstName:    data["first_name"],
 		LastName:     data["last_name"],
 		Email:        data["email"],
-		Password:     password,
 		IsAmbassador: false,
 	}
+	user.SetPassword(data["password"])
 
 	database.DB.Create(&user)
 
@@ -57,7 +54,7 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+	if err := user.ComparePassword(data["password"]); err != nil {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON(fiber.Map{
 			"message": "Invalid Credentials",
