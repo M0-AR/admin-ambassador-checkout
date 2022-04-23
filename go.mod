2fe4056d4268edaa7db49beca62cf73ea121650a
m0-ar <s176492@student.dtu.dk>
2022-04-23 13:00:03 +0200

    JWT

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module admin-ambassador-checkout
 go 1.17
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gofiber/fiber/v2 v2.32.0
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"admin-ambassador-checkout/src/database"
 	"admin-ambassador-checkout/src/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -17,6 +18,10 @@ func main() {
 	database.AutoMigrate()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true, // Allow the frontend to get Cookies from the backend
+	}))
 
 	routes.Setup(app)
 
