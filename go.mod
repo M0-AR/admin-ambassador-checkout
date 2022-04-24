3096ebdf9d966eef272b09fd403894438c79bf7b
m0-ar <s176492@student.dtu.dk>
2022-04-24 15:45:10 +0200

    Add Redis

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/bxcodec/faker/v3 v3.8.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/gofiber/fiber/v2 v2.32.0
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
--- main.go
+++ main.go
@@ -8,14 +8,9 @@ import (
 )
 
 func main() {
-	//user := os.Getenv("MySQL_USER")
-	//pass := os.Getenv("MYSQL_PASSWORD")
-	//host := os.Getenv("MYSQL_HOST") //Here!!
-	//dbname := os.Getenv("MYSQL_DATABASE")
-	//connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, dbname) //Fix!!
-	//_, err := gorm.Open(mysql.Open(connection))
 	database.Connect()
 	database.AutoMigrate()
+	database.SetupRedis()
 
 	app := fiber.New()
 
