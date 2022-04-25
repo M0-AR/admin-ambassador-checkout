fef6f21191cf3140200693639a33849a1bbbde3a
m0-ar <s176492@student.dtu.dk>
2022-04-25 18:59:51 +0200

    Bug fixing

--- src/controllers/ProductController.go
+++ src/controllers/ProductController.go
@@ -91,5 +91,31 @@ func ProductsFrontend(c *fiber.Ctx) error {
 		json.Unmarshal([]byte(result), &products)
 	}
 
-	return c.JSON(result)
+	return c.JSON(products)
 }
+
+//func ProductsFrontend(c *fiber.Ctx) error {
+//	var products []models.Product
+//	var ctx = context.Background()
+//
+//	result, err := database.Cache.Get(ctx, "products_frontend").Result()
+//
+//	if err != nil {
+//		database.DB.Find(&products)
+//
+//		bytes, err := json.Marshal(products)
+//
+//		if err != nil {
+//			panic(err)
+//		}
+//
+//		if errKey := database.Cache.Set(ctx, "products_frontend", bytes, 30*time.Minute).Err(); errKey != nil {
+//			panic(errKey)
+//		}
+//
+//	} else {
+//		json.Unmarshal([]byte(result), &products)
+//	}
+//
+//	return c.JSON(result)
+//}
