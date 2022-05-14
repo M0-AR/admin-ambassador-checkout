fb992e9f21a8cbe5bac51b97d1890396dba418a4
m0-ar <s176492@student.dtu.dk>
2022-05-14 10:27:14 +0200

    Return all ambassadors users

--- src/controllers/userController.go
+++ src/controllers/userController.go
@@ -3,17 +3,33 @@ package controllers
 import (
 	"admin-ambassador-checkout/src/database"
 	"admin-ambassador-checkout/src/models"
+	"admin-ambassador-checkout/src/services"
 	"context"
+	"encoding/json"
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Ambassador(c *fiber.Ctx) error {
+	response, err := services.UserService.Get("users", c.Cookies("jwt", ""))
+
+	if err != nil {
+		return err
+	}
+
 	var users []models.User
 
-	database.DB.Where("is_ambassador = true").Find(&users)
+	ambassadors := []models.User{}
 
-	return c.JSON(users)
+	json.NewDecoder(response.Body).Decode(&users)
+
+	for _, user := range users {
+		if user.IsAmbassador {
+			ambassadors = append(ambassadors, user)
+		}
+	}
+
+	return c.JSON(ambassadors)
 }
 
 func Rankings(c *fiber.Ctx) error {
